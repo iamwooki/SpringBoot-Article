d5cc47ce6075d866e362b70246771cca0052f22b
iamwooki <iamwooki@naver.com>
2023-08-20 19:45:01 +0900

    add comment entity

--- src/main/java/me/iamwooki/toyproject/entity/Comment.java
+++ src/main/java/me/iamwooki/toyproject/entity/Comment.java
@@ -1,4 +1,29 @@
 package me.iamwooki.toyproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne //해당 댓글 엔티티 여러개가, 하나의 Article에 연관
+    @JoinColumn(name= "article_id") //해당 컬럼명으로 생성(FK)
+    private Article article;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String body;
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,15 @@
+-- article dummy data
 INSERT INTO ARTICLE(title, content) VALUES ('가가가가', '1111'), ('나나나나', '2222'), ('다다다다', '3333');
+
+--article dummy data
+INSERT INTO ARTICLE(title, content) VALUES ('favorite movie?', 'Comment go'), ('favorite food?', 'Comment go'), ('your hobby?', 'Comment go');
+
+-- comment dummy data
+---- No.4 article comment
+INSERT INTO comment(article_id, nickname, body) VALUES ('4','PARK', 'Openhimer'), ('4','HONG', 'Openhimer'), ('4','KWON', 'Openhimer');
+
+---- No.5 article comment
+INSERT INTO comment(article_id, nickname, body) VALUES ('5','PARK', 'chicken'), ('5','HONG', 'meat'), ('5','KWON', 'sushi');
+
+---- No.6 article comment
+INSERT INTO comment(article_id, nickname, body) VALUES ('6','PARK', 'fitness'), ('6','HONG', 'watching movie'), ('6','KWON', 'youtube');
