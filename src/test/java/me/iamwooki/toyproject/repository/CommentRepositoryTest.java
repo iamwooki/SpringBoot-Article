7e313f9589a90990a5676abe7157436e51df53fe
iamwooki <iamwooki@naver.com>
2023-08-20 19:59:35 +0900

    add comment entity

---
+++ src/test/java/me/iamwooki/toyproject/repository/CommentRepositoryTest.java
@@ -0,0 +1,16 @@
+package me.iamwooki.toyproject.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentRepositoryTest {
+
+    @Test
+    void findByArticleId() {
+    }
+
+    @Test
+    void findByNickname() {
+    }
+}
